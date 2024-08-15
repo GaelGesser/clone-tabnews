bdef2db49bcd8622b188c5d0d415ce9ace96b471
Gabriel Henrique Gesser <gabrielhenrig@outlook.com>
2024-08-15 16:49:54 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Eduarda saiba que eu te amo muito!!! Espero que de um sorriso quando ver isso.</h1>
+    return <h1>Eduarda saiba que eu te amo muito!!! Espero que de um sorriso quando ver isso. 🥳</h1>
 }
 
 export default Home;
