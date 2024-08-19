76988a1176fd0b104c5116f630213bf7dd9632e2
Gabriel Henrique Gesser <gabrielhenrig@outlook.com>
2024-08-19 00:08:39 +0000

    adiciona scripts lint:check e lint:fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Eduarda saiba que eu te amo muito!!! Espero que de um sorriso quando ver isso. 🥳</h1>
+  return (
+    <h1>
+      Eduarda saiba que eu te amo muito!!! Espero que de um sorriso quando ver
+      isso. 🥳
+    </h1>
+  );
 }
 
 export default Home;
