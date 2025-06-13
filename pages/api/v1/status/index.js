4ee1ff089d6aa88e290d90d4f22a4318b12ea16f
GaelGesser <gabrielhenrig@outlook.com>
2025-06-13 14:26:27 -0300

    add `database.js` and `compose.yaml`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT NOW() AS resultado");
+
+  console.log(result.rows[0].resultado);
   response.status(200).json({ message: "Minha primeira API em Next.js!" });
 }
 
